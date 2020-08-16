b8fcc99b592f8c59c8d2f4e0073d5f81bdff2b81
jaipaljadeja <jadejajaipal5@gmail.com>
2020-08-16 18:52:23 +0530

    Added Popup Card(Un-linked)-Gradients added-more

---
+++ main.js
@@ -0,0 +1,31 @@
+//get modal element
+var modal = document.getElementById("simpleModal");
+//get open modal button
+var modalBtn = document.getElementById("modalBtn");
+
+//get close button
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+//listen for open click
+modalBtn.addEventListener("click", openModal);
+//listen for close click
+closeBtn.addEventListener("click", closeModal);
+//listen for outside click
+window.addEventListener("click", clickOutside);
+
+//function to open modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+//function to close modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+//function to close modal if clicked outside
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
